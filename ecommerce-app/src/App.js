61a3c9a5419046f32b2ecdefad589db710c8949d
Vlad-Bosovets <57301100+Vlad-Bosovets@users.noreply.github.com>
2021-09-27 22:47:31 -0400

    Login popup

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Popup from './components/Popup';
+import { useState } from 'react';
 import './App.css'
 
 function HomeBackground(){
@@ -13,20 +15,36 @@ function HomeBackground(){
 }
 
 function Button() {
+    const [buttonPopup, setButtonPopup] = useState(false);
+    
     return (
         <div>
             <button class="black">
                 Home
             </button>
-            <button>
+            <button onClick={() => setButtonPopup(true)}>
                 Login
             </button>
+
+            <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                <h2>Login</h2>
+                <form>
+                    <label>
+                        Username: <input type="text" name="username" />
+                    </label>
+                    <label>
+                        Password: <input type="password" name="password" />
+                    </label>
+                </form>
+            </Popup>
+
             <button class="orange">
                 Sign Up
             </button>
         </div> 
     );
 }
+
 export default function App() {
     return (
         <HomeBackground />
