7c65cb92160e2242f0bfa9e9e47027246dfbf8f2
HasanulRafi <hasanulrafi46@gmail.com>
2021-09-27 23:10:39 -0400

    Done with adding products

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 import logo from './images/Logo.png';
+import book1 from './images/book1.jpg';
+import book2 from './images/book2.jpg';
+import book3 from './images/book3.jpg';
+import book4 from './images/book4.jpg';
+import book5 from './images/book5.jpg';
+import book6 from './images/book6.jpg';
+
 
 function HomeBackground(){
     return(
@@ -13,6 +20,22 @@ function HomeBackground(){
         </>
     );
 }
+
+function Products(){
+    return (
+        <div>
+            <h1>Book Adds</h1>
+                <img src={book1} alt=""/> 
+                <img src={book2} alt=""/> 
+                <img src={book3} alt=""/> 
+                <img src={book4} alt=""/> 
+                <img src={book5} alt=""/> 
+                <img src={book6} alt=""/> 
+        </div> 
+    );
+}
+
+//export default images;
 
 function Button() {
     return (
@@ -31,6 +54,9 @@ function Button() {
 }
 export default function App() {
     return (
+        <div>
         <HomeBackground />
+        <Products />
+        </div>
     );
 }
