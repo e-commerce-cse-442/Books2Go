8106fcbfd3391491b3fcfeb9e1d0bc7ccb9c7bde
danielip <danielip@buffalo.edu>
2021-10-01 00:45:04 -0400

    Changed process port vs local host port.

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,15 @@ const path = require('path')
 
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const HOST = '0.0.0.0';
 
 
-
-// app.get('/', (req, res) => {
-//   let reqPath = path.join(__dirname, '../');
-//
-//   res.sendFile(path.join(reqPath + 'public/index.html'));
-// });
-//
 console.log(path.join(__dirname, "..", "build"));
 app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.use(express.static('../public'));
-// app.use(express.static('../src'));
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
