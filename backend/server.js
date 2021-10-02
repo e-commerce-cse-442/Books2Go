c5b5bf163f7fa160b0bf107745913eed91dbb7cc
Daniel <danielip@buffalo.edu>
2021-10-02 00:48:56 -0400

    Testing for database

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,24 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const HOST = '0.0.0.0';
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT books2go, user_id FROM User_Info;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 console.log(path.join(__dirname, "..", "build"));
 app.use(express.static(path.join(__dirname, "..", "build")));
