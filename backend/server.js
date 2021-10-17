580373953af4a4e65ccf220974c5912b183be932
BuildAllTogether <47541335+BuildAllTogether@users.noreply.github.com>
2021-10-17 16:23:52 -0400

    Pull data from developing

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const cors = require("cors");
+const client = require("./db");
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
-const HOST = '0.0.0.0';
+client.connect();
 
 const { Client } = require('pg');
 
@@ -17,9 +18,6 @@ const client = new Client({
 
 client.connect();
 
-console.log("DATABASE URL IS PRINTING: ", process.env.DATABASE_URL)
-console.log("PROCESS PORT: ", process.env.PORT)
-
 client.query('SELECT * FROM user_info ', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
@@ -28,11 +26,36 @@ client.query('SELECT * FROM user_info ', (err, res) => {
   client.end();
 });
 
-console.log(path.join(__dirname, "..", "build"));
 app.use(express.static(path.join(__dirname, "..", "build")));
-
 app.use(express.static('../public'));
 
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//create
+app.post("/signup", async(req, res) =>{
+    //async: wait for the function
+    try{
+        const name = req.body.username;
+        const password = req.body.password;
+        const email = req.body.email;
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const post = await client.query(
+            `INSERT INTO User_Info (username, user_first_name, user_last_name, user_password, user_email) VALUES('${name}', '${firstName}', '${lastName}' , '${password}', '${email}') RETURNING *`
+        );
+        res.json(post.rows[0]);
+
+    } catch(err){
+        console.error(err.message);
+    }
+});
+
+
+
+const PORT = process.env.PORT || 8000;
+const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
