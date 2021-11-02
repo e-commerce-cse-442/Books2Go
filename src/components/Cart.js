505ecf15b5633a786be24c73bfe24cda8411a120
BuildAllTogether <47541335+BuildAllTogether@users.noreply.github.com>
2021-11-02 14:22:11 -0400

    HtML and CSS for Cart #69

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Navbar from './Navbar'
+import "./Cart.css"
 
 function Cart() {
     return (
         <div>
             <Navbar />
-            <h1>implement the cart here</h1>
-        </div>
+            <div class="your-cart">
+              <h1 class="cart-head">Your Cart</h1>
+              <span class= "item-count">(0 Items)</span>
+            </div>
+            <div class="cart-row">
+                <span class="cart-item cart-header cart-column">Item</span>
+                <span class="cart-price cart-header cart-column">Price</span>
+                <span class="cart-quantity cart-header cart-column">Quantity</span>
+            </div>
+            <div class="cart-items"></div>
+            <div class="cart-total">
+                <strong class="cart-total-title">Total</strong>
+                <span class="cart-total-price">$0</span>
+            </div>
+            <button class="button-checkout">Checkout</button>
+            </div>
     )
 }
 
