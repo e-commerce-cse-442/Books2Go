4e310b166259781f846bd010d3fe83534c586f77
mhkhan4 <mhkhan4@buffalo.edu>
2021-11-04 22:30:09 -0400

    making the marketplace page dynamic #70

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "./Product.css";
-import book1 from "../images/book1.jpg";
-import book2 from "../images/book2.jpg";
-import book4 from "../images/book4.jpg";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Product() {
@@ -25,7 +22,7 @@ function Product() {
             </div>
             <div class="text-center">
               {" "}
-              <img src={book1} alt="" width="250" />{" "}
+              <img src="images/book1.jpg" alt="" width="250" />{" "}
             </div>
             <div class="text-center">
               <h5>Fundamentals of Python</h5>{" "}
@@ -45,7 +42,7 @@ function Product() {
             </div>
             <div class="text-center">
               {" "}
-              <img src={book4} alt="" width="250" />{" "}
+              <img src="images/book4.jpg" alt="" width="250" />{" "}
             </div>
             <div class="text-center">
               <h5>Think and grow rich</h5>{" "}
@@ -64,7 +61,7 @@ function Product() {
             </div>
             <div class="text-center">
               {" "}
-              <img src={book2} alt="" width="250" />{" "}
+              <img src="images/book2.jpg" alt="" width="250" />{" "}
             </div>
             <div class="text-center">
               <h5>Algorithm Design</h5> <span class="text-success">FREE</span>
