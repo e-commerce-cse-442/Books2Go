a99e6821e71c6eac89f32f8de6e56008e15b1492
BuildAllTogether <47541335+BuildAllTogether@users.noreply.github.com>
2021-11-07 03:06:24 -0500

    Database update #39

--- database_scripts/books2go.sql
+++ database_scripts/books2go.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS User_Info (
+CREATE TABLE IF NOT EXISTS user_info (
 	user_id SERIAL PRIMARY KEY,
 	username varchar(255) UNIQUE,
 	user_first_name varchar(100),
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS User_Info (
   user_city	varchar(50),
   user_state	varchar(50),
   user_zip_code	int,
-  user_country	varchar(50)
+  user_country	varchar(50),
+	cart_id SERIAL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS discount (
@@ -32,22 +33,21 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 CREATE TABLE IF NOT EXISTS cart (
-  cart_id	  SERIAL PRIMARY KEY,
-  user_id	  int REFERENCES User_Info(user_id),
+  cart_id	  int REFERENCES user_info(cart_id),
   total_cost	int
 );
 
 CREATE TABLE IF NOT EXISTS cart_items (
   cart_items_id	  SERIAL PRIMARY KEY,
   book_id	  int REFERENCES books(book_id),
-  cart_id	  int REFERENCES cart(cart_id),
+  cart_id	  int REFERENCES user_info(cart_id),
   quantity	int
 );
 
 CREATE TABLE IF NOT EXISTS order_info (
   order_id	SERIAL PRIMARY KEY,
   total_cost	int,
-  user_id	int REFERENCES User_Info(user_id)
+  user_id	int REFERENCES user_info(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS order_items (
