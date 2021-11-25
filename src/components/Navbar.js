a3c1cdcb6d3087a913ce557c4acb56e0ff026d8b
Vlad-Bosovets <57301100+Vlad-Bosovets@users.noreply.github.com>
2021-11-24 20:02:28 -0500

    issue #99 resolved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useCookies } from 'react-cookie';
-import username from "./Login";
 
 
 function Navbar() {
@@ -41,11 +40,6 @@ function Navbar() {
                   Shop
               </Link>
             </li>
-            {/* <li class="nav-item">
-              <Link to="/pdfBook" class="nav-link" href="#">
-                PDF Books
-              </Link>
-            </li> */}
             <li class="nav-item">
               <Link to="/cart"class="nav-link" href="#">
                 Cart
@@ -53,14 +47,6 @@ function Navbar() {
             </li>
           </ul>
           <form class="form-inline my-2 my-lg-0">
-            {/* <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-            />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button> */}
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
             <li class="nav-item active">
               <Link to="/signup" class="nav-link" href="#">
